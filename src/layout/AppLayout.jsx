2757b6e271061c56b8ba93fa29ba85fb0b7f7987
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-23 13:24:08 +0200

    feat: Add Sidebar to main layout and outlet for all routes

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,12 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+import Sidebar from "../ui/Sidebar";
+
 export default function AppLayout() {
 	return (
-		<div className="grid h-screen grid-cols-[20rem_1fr] grid-rows-[auto_1fr]">
+		<div className="grid h-screen grid-cols-[18rem_1fr] grid-rows-[auto_1fr]">
+			<Sidebar />
 			<header className="col-span-1 bg-green-300">HEADER</header>
-			<aside className="col-start-1 col-end-2 row-span-full bg-red-300">
-				SIDEBAR
-			</aside>
 			<main className="overflow-scroll bg-orange-300 p-10">
-				<div className="max-w-[120rem] bg-purple-300">MAIN</div>
+				<div className="max-w-[120rem] bg-purple-300">
+					<Outlet />
+				</div>
 			</main>
 		</div>
 	);
