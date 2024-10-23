4e9b34a83476705639571dffc042c7d02678fb5e
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-23 13:08:12 +0200

    feat: Add main page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import AppLayout from "./layout/AppLayout";
+import Dashboard from "./pages/Dashboard";
+import Goals from "./pages/Goals";
+import Journal from "./pages/Journal";
+import Habits from "./pages/Habits";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <AppLayout />
+		element: <AppLayout />,
+		children: [
+			{
+				path: "/dashboard",
+				element: <Dashboard />
+			},
+			{
+				path: "/goals",
+				element: <Goals />
+			},
+			{
+				path: "/journal",
+				element: <Journal />
+			},
+			{
+				path: "/habits",
+				element: <Habits />
+			}
+		]
 	}
 ]);
 
