d060d622a7e040d3fad6c4040ee5558d6df21ee4
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-23 13:30:17 +0200

    feat: Add header to main layout | style: Change bg colour of main element

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 
 import Sidebar from "../ui/Sidebar";
+import Header from "../ui/Header";
 
 export default function AppLayout() {
 	return (
 		<div className="grid h-screen grid-cols-[18rem_1fr] grid-rows-[auto_1fr]">
 			<Sidebar />
-			<header className="col-span-1 bg-green-300">HEADER</header>
-			<main className="overflow-scroll bg-orange-300 p-10">
-				<div className="max-w-[120rem] bg-purple-300">
+			<Header />
+			<main className="bg-body-bgLight overflow-scroll p-10">
+				<div className="max-w-[120rem]">
 					<Outlet />
 				</div>
 			</main>
