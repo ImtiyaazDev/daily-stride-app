3ff292a5a2b38501d043afa890d2da66f7fe9f8e
ImtiyaazDev <Imtiyaazmac@gmail.com>
2024-10-23 13:07:34 +0200

    chore: Create list of sidebar links

---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const sidebarLinks = [
+	{
+		id: 1,
+		text: "Dashboard",
+		to: "/dashboard"
+	},
+	{
+		id: 2,
+		text: "Goals",
+		to: "/goals"
+	},
+	{
+		id: 1,
+		text: "Journal",
+		to: "/journal"
+	},
+	{
+		id: 1,
+		text: "Habits",
+		to: "/habits"
+	}
+];
+
+export { sidebarLinks };
